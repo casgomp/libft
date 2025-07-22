85f66a5f50a6e52b27c0c94de0f75c45feddc1ba
casgomp <pecastro@c3a2c9.42berlin.de>
2025-07-22 11:10:12 +0200

    new libft structure for ease of use in other projects

---
+++ include/ft_printf.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pecastro <pecastro@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/04 18:57:51 by pecastro          #+#    #+#             */
+/*   Updated: 2025/06/04 19:16:03 by pecastro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stdint.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft.h"
+
+int	ft_printf(const char *format, ...);
+int	ft_putchar_prnt(int c, int *count_chars);
+int	ft_putstr_prnt(void *str, int *count_chars);
+int	ft_putnbr_base_prnt(int nb, int *count_chars, char *base);
+int	ft_putnbr_base_prnt_address(uintptr_t nb, int *count_chars, char *base);
+int	ft_putnbr_base_prnt_address_pre(uintptr_t nb, int 
+		*count_chars, int prefix, int first_call);
+
+#endif
