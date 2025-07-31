608bfa3ae84a245a21f85a99c4e095fdd0b16316
casgomp <pecastro@c4b8c6.42berlin.de>
2025-07-31 10:36:49 +0200

    added ft_atoi_safe

--- include/ft_string.h
+++ include/ft_string.h
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_string.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pecastro <pecastro@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/24 11:52:16 by pecastro          #+#    #+#             */
+/*   Updated: 2025/07/31 10:26:53 by pecastro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #ifndef FT_STRING_H
 # define FT_STRING_H
 
-#include <stddef.h>
+# include <stddef.h>
 
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *nptr);
+int		ft_atoi_safe(const char *nptr);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
